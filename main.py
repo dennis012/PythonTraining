c5e9ac59b3b30cab40853809f209930b11f0bd43
denzoh <dennisndungu68@gmail.com>
2018-12-12 11:53:57 +0300

    the variables exercise

---
+++ main.py
@@ -0,0 +1,20 @@
+Genre = "Reggae"
+Artist = "Bob Marley"
+Year_Released = "1999"
+#the duration in seconds
+Duration = 329
+#the total views in youtube
+Views = 5000000
+ProducerName = "Musyoka"
+Title = "3 Little Birds"
+ArtistPlaceOfOrigin = "Jamaica"
+"""outputing the different attributes 
+ of the song liked by the student """
+print(Genre)
+print(Artist)
+print(Year_Released)
+print(Duration)
+print(Views)
+print(ProducerName)
+print(Title)
+print(ArtistPlaceOfOrigin)
---
+++ trial.py
@@ -0,0 +1,17 @@
+PI = 3.14
+
+one = 1
+two = 2
+three = 3
+print(PI* two)
+print(one)
+print(two)
+print(three)
+print(two * three)
+
+Decimal = 1.1
+print(Decimal)
+
+def trial():
+    web = 1
+    return web
