16649760f2d3ef21ef0fffd0d08660571b1e9a64
denzoh <dennisndungu68@gmail.com>
2018-12-13 12:53:53 +0300

    functions section

---
+++ functions.py
@@ -0,0 +1,25 @@
+def genre():
+    name = "Reggae"
+    return name
+
+def artist():
+    name = "Bob Marley"
+    return name
+
+def year():
+    release_date = 1999
+    return release_date
+
+def title():
+    isJamaica = True
+    isNotJamaica = False
+    return isJamaica
+
+release_year = year()
+print(release_year)
+type = genre()
+print(type)
+name = artist()
+print(name)
+song = title()
+print(song)
--- trial.py
+++ trial.py
@@ -20,3 +20,37 @@ a = 20
 b = 20
 output = trial(a,b)
 print(output)
+
+def division(number1,number2):
+    division = number1/number2
+    return division
+
+a = 56
+b = 8
+result = division(a,b)
+print(result)
+
+def hello():
+    print("I am a beginner")
+hello()
+hello()
+
+def sumA(count):
+    a = count + 1
+    return a
+a = sumA(2)
+print(a)
+
+
+def printmessage(message, ntimes=1):
+    print(message * ntimes)
+
+printmessage("Hello")
+printmessage("Hello", 5)
+
+num = 1
+def func():
+     num = 4
+     print(num)
+func()
+print(num)
